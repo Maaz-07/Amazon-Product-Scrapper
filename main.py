4245503b6f17e1e96354fc4a6cbb974d83672ad5
Maaz-07 <maazchoudhry07@gmail.com>
2023-07-03 20:51:18 +0500

    Single Product Print

--- main.py
+++ main.py
@@ -1,48 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_amazon(search_query):
-    base_url = "https://www.amazon.com"
-    search_url = base_url + "/s?k=" + search_query.replace(" ", "+")
+def get_product_name(soup):
+    try:
+        name = soup.find("span", attrs={"class": "a-size-medium a-color-base a-text-normal"}).text.strip()
+    except AttributeError:
+        name = ""
+    return name
 
-    # Send a GET request to the search URL
-    response = requests.get(search_url)
+def get_product_price(soup):
+    try:
+        price = soup.find("span", attrs={"class": "a-offscreen"}).text.strip()
+    except AttributeError:
+        price = ""
+    return price
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, "html.parser")
+def get_product_rating(soup):
+    try:
+        rating = soup.find("span", attrs={"class": "a-icon-alt"}).text.strip()
+    except AttributeError:
+        rating = ""
+    return rating
 
-    # Find all the product containers on the page
-    product_containers = soup.find_all("div", {"data-component-type": "s-search-result"})
+def get_number_of_raters(soup):
+    try:
+        raters = soup.find("span", attrs={"class": "a-size-base"}).text.strip()
+    except AttributeError:
+        raters = ""
+    return raters
 
-    # Extract desired information from each product container
-    for container in product_containers:
-        # Extract the product title
-        title_element = container.find("span", class_="a-size-medium")
-        if title_element:
-            title = title_element.text.strip()
-        else:
-            title = "N/A"
+def get_availability(soup):
+    try:
+        availability = soup.find("span", attrs={"class": "a-size-medium a-color-success"}).text.strip()
+    except AttributeError:
+        availability = "Not Available"
+    return availability
 
-        # Extract the product price
-        price_element = container.find("span", class_="a-offscreen")
-        if price_element:
-            price = price_element.text.strip()
-        else:
-            price = "N/A"
+if __name__ == '__main__':
 
-        # Extract the product rating
-        rating_element = container.find("span", class_="a-icon-alt")
-        if rating_element:
-            rating = rating_element.text.strip()
-        else:
-            rating = "N/A"
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0', 'Accept-Language': 'en-US, en;q=0.5'}
 
-        # Print the extracted information
-        print("Title:", title)
-        print("Price:", price)
-        print("Rating:", rating)
-        print("-----------------------")
+    URL = "https://www.amazon.com/s?k=laptop&s=exact-aware-popularity-rank&qid=1688399269&ref=sr_st_exact-aware-popularity-rank&ds=v1%3ACi97C3YM1IuUrOkVaNbdCecAUUc7gn0xMLw0v1%2BPFNk"
 
-# Example usage
-search_query = "laptop"
-scrape_amazon(search_query)
+    webpage = requests.get(URL, headers=HEADERS)
+
+    soup = BeautifulSoup(webpage.content, "html.parser")
+
+    name = get_product_name(soup)
+    price = get_product_price(soup)
+    rating = get_product_rating(soup)
+    raters = get_number_of_raters(soup)
+    availability = get_availability(soup)
+
+    print("Product Name:", name)
+    print("Price:", price)
+    print("Rating:", rating)
+    print("Number of Raters:", raters)
+    print("Availability:", availability)
