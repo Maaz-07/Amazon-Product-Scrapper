7e3222889fe208559fac6ffb3167f7886c28bcaf
Maaz-07 <maazchoudhry07@gmail.com>
2023-07-03 22:13:57 +0500

    Scrap First Page

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-def get_product_name(soup):
-    try:
-        name = soup.find("span", attrs={"class": "a-size-medium a-color-base a-text-normal"}).text.strip()
-    except AttributeError:
-        name = ""
-    return name
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
+    'Accept-Language': 'en-US, en;q=0.5'
+}
 
-def get_product_price(soup):
-    try:
-        price = soup.find("span", attrs={"class": "a-offscreen"}).text.strip()
-    except AttributeError:
-        price = ""
-    return price
+URL = "https://www.amazon.com/s?k=laptop&s=exact-aware-popularity-rank&qid=1688399269&ref=sr_st_exact-aware-popularity-rank&ds=v1%3ACi97C3YM1IuUrOkVaNbdCecAUUc7gn0xMLw0v1%2BPFNk"
 
-def get_product_rating(soup):
-    try:
-        rating = soup.find("span", attrs={"class": "a-icon-alt"}).text.strip()
-    except AttributeError:
-        rating = ""
-    return rating
+webpage = requests.get(URL, headers=HEADERS)
+soup = BeautifulSoup(webpage.content, "html.parser")
 
-def get_number_of_raters(soup):
-    try:
-        raters = soup.find("span", attrs={"class": "a-size-base"}).text.strip()
-    except AttributeError:
-        raters = ""
-    return raters
+links = soup.find_all("a", attrs={'class': 'a-link-normal s-no-outline'})
+links_list = []
+for link in links:
+    href = link.get('href')
+    if href.startswith('/'):
+        product_page = requests.get('https://www.amazon.com' + href, headers=HEADERS)
+        product_soup = BeautifulSoup(product_page.content, "html.parser")
+        sponsored_element = product_soup.find("span", attrs={'class': 'a-color-base'}, string='Sponsored')
+        if not sponsored_element:
+            links_list.append('https://www.amazon.com' + href)
 
-def get_availability(soup):
-    try:
-        availability = soup.find("span", attrs={"class": "a-size-medium a-color-success"}).text.strip()
-    except AttributeError:
-        availability = "Not Available"
-    return availability
+for link in links_list:
+    product_page = requests.get(link, headers=HEADERS)
+    product_soup = BeautifulSoup(product_page.content, "html.parser")
 
-if __name__ == '__main__':
+    # Extract the name
+    name = product_soup.find("span", attrs={'id': 'productTitle'}).text.strip()
 
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0', 'Accept-Language': 'en-US, en;q=0.5'}
+    # Extract the price
+    price = product_soup.find("span", attrs={'class': 'a-offscreen'}).text.strip()
 
-    URL = "https://www.amazon.com/s?k=laptop&s=exact-aware-popularity-rank&qid=1688399269&ref=sr_st_exact-aware-popularity-rank&ds=v1%3ACi97C3YM1IuUrOkVaNbdCecAUUc7gn0xMLw0v1%2BPFNk"
+    # Extract the rating
+    rating = product_soup.find("span", attrs={'class': 'a-icon-alt'}).text.strip()
 
-    webpage = requests.get(URL, headers=HEADERS)
+    # Extract the number of raters
+    num_raters = product_soup.find("span", attrs={'id': 'acrCustomerReviewText'}).text.strip()
 
-    soup = BeautifulSoup(webpage.content, "html.parser")
+    # Extract the availability
+    availability_element = product_soup.find("span", attrs={'class': 'a-size-medium a-color-success'})
+    availability = availability_element.text.strip() if availability_element else 'Not available'
 
-    name = get_product_name(soup)
-    price = get_product_price(soup)
-    rating = get_product_rating(soup)
-    raters = get_number_of_raters(soup)
-    availability = get_availability(soup)
+    # Extract the specific description
+    description_table = product_soup.find("table", attrs={'class': 'a-normal a-spacing-micro'})
+    rows = description_table.find_all("tr")
+    description_data = {}
+    for row in rows:
+        cells = row.find_all("td")
+        if len(cells) == 2:
+            attribute = cells[0].find("span").text.strip()
+            value = cells[1].find("span").text.strip()
+            description_data[attribute] = value
 
-    print("Product Name:", name)
+    print("Name:", name)
     print("Price:", price)
     print("Rating:", rating)
-    print("Number of Raters:", raters)
+    print("Number of raters:", num_raters)
     print("Availability:", availability)
+    print("Description:", description_data)
+    print("------")
