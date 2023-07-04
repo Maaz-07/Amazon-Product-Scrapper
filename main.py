2792173de11d56ca55d30a045d5d48181951e39b
Maaz-07 <maazchoudhry07@gmail.com>
2023-07-04 22:16:33 +0500

    Save into .xls File

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import openpyxl
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
@@ -22,41 +23,52 @@ for link in links:
         if not sponsored_element:
             links_list.append('https://www.amazon.com' + href)
 
+data = []
+attribute_names = set()
+
 for link in links_list:
     product_page = requests.get(link, headers=HEADERS)
     product_soup = BeautifulSoup(product_page.content, "html.parser")
 
-    # Extract the name
     name = product_soup.find("span", attrs={'id': 'productTitle'}).text.strip()
-
-    # Extract the price
     price = product_soup.find("span", attrs={'class': 'a-offscreen'}).text.strip()
-
-    # Extract the rating
     rating = product_soup.find("span", attrs={'class': 'a-icon-alt'}).text.strip()
-
-    # Extract the number of raters
     num_raters = product_soup.find("span", attrs={'id': 'acrCustomerReviewText'}).text.strip()
-
-    # Extract the availability
     availability_element = product_soup.find("span", attrs={'class': 'a-size-medium a-color-success'})
     availability = availability_element.text.strip() if availability_element else 'Not available'
 
-    # Extract the specific description
     description_table = product_soup.find("table", attrs={'class': 'a-normal a-spacing-micro'})
-    rows = description_table.find_all("tr")
-    description_data = {}
-    for row in rows:
-        cells = row.find_all("td")
-        if len(cells) == 2:
-            attribute = cells[0].find("span").text.strip()
-            value = cells[1].find("span").text.strip()
-            description_data[attribute] = value
+    if description_table:
+        rows = description_table.find_all("tr")
+        description_data = {}
+        for row in rows:
+            cells = row.find_all("td")
+            if len(cells) == 2:
+                attribute = cells[0].find("span").text.strip()
+                value = cells[1].find("span").text.strip()
+                description_data[attribute] = value
+                attribute_names.add(attribute)
+    else:
+        description_data = {}
 
-    print("Name:", name)
-    print("Price:", price)
-    print("Rating:", rating)
-    print("Number of raters:", num_raters)
-    print("Availability:", availability)
-    print("Description:", description_data)
-    print("------")
+    item_data = [name, price, rating, num_raters, availability]
+    for attribute in attribute_names:
+        item_data.append(description_data.get(attribute, ""))
+
+    data.append(item_data)
+
+# Create an Excel workbook and worksheet
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+# Write headers to the worksheet
+headers = ['Name', 'Price', 'Rating', 'Number of Raters', 'Availability']
+headers.extend(attribute_names)
+worksheet.append(headers)
+
+# Write data to the worksheet
+for item in data:
+    worksheet.append(item)
+
+# Save the workbook
+workbook.save("laptop_data.xlsx")
